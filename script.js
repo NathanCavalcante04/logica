8b0b3e86c81636d8424abeb4c3b3943d4ba3211d
nathan <nathan.cavalcante1010@gmail.com>
2022-01-17 23:23:46 -0300

    estrutura

---
+++ script.js
@@ -0,0 +1,15 @@
+let string = "nathan"
+
+let number = 10
+
+let anoAtual = 2022
+
+console.log(anoAtual - number)
+
+console.log("estou mexendo no console.log")
+
+let quartaFeira = true
+
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
